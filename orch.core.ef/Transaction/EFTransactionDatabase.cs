ece10dcac118d77b3e4903bb02d2629e5ee63f31
silviomartignetti <silviomartignetti@gmail.com>
2024-03-21 21:06:21 +0300

    'IsPermittedAny' OViewFunction

--- orch.core.ef/Transaction/EFTransactionDatabase.cs
+++ orch.core.ef/Transaction/EFTransactionDatabase.cs
@@ -943,17 +943,17 @@ namespace orch.core.ef.System
         [OViewFunction("IsPermittedAll")]
         public bool IsPermitted(Guid userId, string[] permissionKeys, out string[] notGrantedPermissions)
         {
-            if (GetRootUser() is UserInfo rootUser && rootUser.Id == userId)
-            {
-                notGrantedPermissions = Array.Empty<string>();
-                return true;
-            }
-
             var permissions = _db.Permissions.Where(x => permissionKeys.Contains(x.PermissionKey)).ToList();
             if (permissions.Count != permissionKeys.Length)
             {
                 var notFoundKeys = permissionKeys.Except(permissions.Select(p => p.PermissionKey));
                 throw new InvalidOperationException($"Permission key(s) {string.Join(", ", notFoundKeys)} not found");
+            }
+
+            if (GetRootUser() is { } rootUser && rootUser.Id == userId)
+            {
+                notGrantedPermissions = Array.Empty<string>();
+                return true;
             }
 
             var userPermissionIds = _db.UserRoles
@@ -977,7 +977,7 @@ namespace orch.core.ef.System
             return isPermitted;
         }
 
-
+        [OViewFunction("IsPermittedAny")]
         public bool IsPermittedAny(Guid userId, params string[] permissionKeys)
         {
             var permissions = _db.Permissions.Where(x => permissionKeys.Contains(x.PermissionKey)).ToList();
@@ -987,6 +987,11 @@ namespace orch.core.ef.System
                 throw new InvalidOperationException($"Permission key(s) {string.Join(", ", notFoundKeys)} not found");
             }
 
+            if (GetRootUser() is { } rootUser && rootUser.Id == userId)
+            {
+                return true;
+            }
+            
             var userPermissionIds = _db.UserRoles
                 .Where(x => x.UserId == userId) // select the roles of the user
                 .Join(_db.PermissionRoles, a => a.RoleId, b => b.RoleId, (a, b) => b) // join with permission roles table
