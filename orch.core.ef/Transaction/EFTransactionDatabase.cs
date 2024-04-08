5fe1cf7a9036d43e312413a558e48230cdf00281
Silvio Martignetti <silviomartignetti@gmail.com>
2024-04-08 17:21:32 +0300

    did jobs ever really work?

--- orch.core.ef/Transaction/EFTransactionDatabase.cs
+++ orch.core.ef/Transaction/EFTransactionDatabase.cs
@@ -23,8 +23,12 @@ namespace orch.core.ef.System
 
         private DbTransaction? _dbTransaction;
 
-        public EFTransactionDatabase(OTransactionDbContext db, ISystemDatabase systemDatabase) =>
-            (_db, _contexts, _systemDatabase) = (db, new List<ODbContext>(), systemDatabase);
+        public EFTransactionDatabase(OTransactionDbContext db, ISystemDatabase systemDatabase)
+        {
+            _db = db;
+            _contexts = new List<ODbContext>();
+            _systemDatabase = systemDatabase;
+        }
 
         /// <summary>
         /// Adds the specified <paramref name="context"/> to the list of database contexts to be included in the transaction.
