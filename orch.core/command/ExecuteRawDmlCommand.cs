1dbab0390c4b57a55d076d5eea57272c34d44f22
Silvio Martignetti <silviomartignetti@gmail.com>
2024-09-29 20:44:29 +0300

    added optional 'Note' to 'SYS_EXEC_RAW_DML'

--- orch.core/command/ExecuteRawDmlCommand.cs
+++ orch.core/command/ExecuteRawDmlCommand.cs
@@ -11,6 +11,7 @@
         public const string COMMAND_TYPE_KEY = "SYS_EXEC_RAW_DML";
         public const string TYPE_ID = "6ce34702-61ad-44e6-bbad-5ca4734cde5a";
         public string Dml { get; set; }
+        public string? Note { get; set; }
     }
 
     public class ExecuteRawDmlCommandInitializer : CommandInitializerBase<ExecuteRawDmlCommand>
@@ -21,10 +22,12 @@
 
         public override void Init()
         {
-            if (string.IsNullOrEmpty(_commandData.Dml))
+            if (string.IsNullOrEmpty(_commandData.Dml.Trim()))
             {
                 throw new InvalidOperationException("DML cannot be null or empty.");
             }
+
+            _commandData.Note = _commandData.Note?.Trim();
         }
     }
 
@@ -34,13 +37,33 @@
         {
         }
 
+        protected override void Authorize()
+        {
+            if (_commandInfo.UserId is not { } userId || _services.TranDb.GetRootUser()?.Id != userId)
+            {
+                throw new InvalidOperationException("Only root user can execute this command.");
+            }
+        }
+
         public override void Preprocess()
         {
+            var lowerDml = _commandData.Dml.ToLower();
+            if (lowerDml.Contains("begin") || lowerDml.Contains("commit") || lowerDml.Contains("rollback"))
+            {
+                throw new InvalidOperationException(
+                    "DML should not contain transaction statements (BEGIN, COMMIT, ROLLBACK).");
+            }
         }
 
         public override string Summarize(out bool html)
         {
             html = false;
+
+            if (!string.IsNullOrEmpty(_commandData.Note))
+            {
+                return $"Executed raw DML ({_commandData.Note}): {_commandData.Dml}";
+            }
+            
             return $"Executed raw DML: {_commandData.Dml}";
         }
 
